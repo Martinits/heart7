bd007849d018c8dc5863ff901c67cda595839318
Martinits <mamartinitramam@163.com>
2023-08-10 20:55:35 +0800

    proto definition

--- src/bin/client.rs
+++ src/bin/client.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use heart7::heart7_client::Heart7Client;
+use heart7::{PlayerInfo, RoomInfo};
+
+pub mod heart7 {
+    tonic::include_proto!("heart7");
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut client = Heart7Client::connect("http://localhost:20007").await?;
+
+    let request = tonic::Request::new(PlayerInfo {
+        name: "Martinits".into(),
+    });
+
+    let response = client.new_room(request).await?;
+
+    println!("RESPONSE={:?}", response);
+
+    Ok(())
 }
--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,3 +1,180 @@
-fn main() {
-    println!("Hello, world!");
+use tonic::{transport::Server, Request, Response, Status};
+use heart7::{*, heart7_server::*};
+
+pub mod heart7 {
+    tonic::include_proto!("heart7");
+}
+
+#[derive(Debug, Default)]
+pub struct Heart7D {}
+
+impl Heart7D {
+    fn new_card(&self) -> Card {
+        Card {
+            suit: CardSuit::Spade as i32,
+            num: 1,
+        }
+    }
+
+    fn new_chain(&self) -> Chain {
+        Chain {
+            null: true,
+            head: Some(self.new_card()),
+            tail: Some(self.new_card()),
+            head_thisround: 0,
+            tail_thisround: 0,
+        }
+    }
+
+}
+
+#[tonic::async_trait]
+impl Heart7 for Heart7D {
+    async fn new_room(
+        &self,
+        request: Request<PlayerInfo>,
+    ) -> Result<Response<RoomInfo>, Status> {
+
+        println!("Got a request: {:?}", request);
+
+        let reply = RoomInfo {
+            roomid: format!("Hello!").into(),
+            players: Vec::new(),
+            state: RoomState::NotFull as i32,
+        };
+
+        Ok(Response::new(reply))
+    }
+
+    async fn join_room(
+        &self,
+        request: Request<RoomReq>,
+    ) -> Result<Response<RoomInfo>, Status> {
+
+        println!("Got a request: {:?}", request);
+
+        let reply = RoomInfo {
+            roomid: format!("Hello!").into(),
+            players: Vec::new(),
+            state: RoomState::NotFull as i32,
+        };
+
+        Ok(Response::new(reply))
+    }
+
+    async fn room_status(
+        &self,
+        request: Request<RoomReq>,
+    ) -> Result<Response<RoomInfo>, Status> {
+
+        println!("Got a request: {:?}", request);
+
+        let reply = RoomInfo {
+            roomid: format!("Hello!").into(),
+            players: Vec::new(),
+            state: RoomState::NotFull as i32,
+        };
+
+        Ok(Response::new(reply))
+    }
+
+    async fn game_ready(
+        &self,
+        request: Request<RoomReq>,
+    ) -> Result<Response<CommonReply>, Status> {
+
+        println!("Got a request: {:?}", request);
+
+        let reply = CommonReply {
+            success: true,
+            msg: "Ok".into(),
+        };
+
+        Ok(Response::new(reply))
+    }
+
+    async fn game_status(
+        &self,
+        request: Request<RoomReq>,
+    ) -> Result<Response<GameInfo>, Status> {
+
+        println!("Got a request: {:?}", request);
+
+        let reply = GameInfo {
+            state: GameState::Notready as i32,
+            ready: Vec::new(),
+            cards: Vec::new(),
+            waitfor: 0,
+            desk: Some(Desk {
+                spade: Some(self.new_chain()),
+                heart: Some(self.new_chain()),
+                club: Some(self.new_chain()),
+                diamond: Some(self.new_chain()),
+            }),
+            held: Some(HeldCards {
+                my: Vec::new(),
+                others: Vec::new(),
+            }),
+        };
+
+        Ok(Response::new(reply))
+    }
+
+    async fn play_card(
+        &self,
+        request: Request<PlayReq>,
+    ) -> Result<Response<CommonReply>, Status> {
+
+        println!("Got a request: {:?}", request);
+
+        let reply = CommonReply {
+            success: true,
+            msg: "Ok".into(),
+        };
+
+        Ok(Response::new(reply))
+    }
+
+    async fn exit_game(
+        &self,
+        request: Request<RoomReq>,
+    ) -> Result<Response<CommonReply>, Status> {
+
+        println!("Got a request: {:?}", request);
+
+        let reply = CommonReply {
+            success: true,
+            msg: "Ok".into(),
+        };
+
+        Ok(Response::new(reply))
+    }
+
+    async fn exit_room(
+        &self,
+        request: Request<RoomReq>,
+    ) -> Result<Response<CommonReply>, Status> {
+
+        println!("Got a request: {:?}", request);
+
+        let reply = CommonReply {
+            success: true,
+            msg: "Ok".into(),
+        };
+
+        Ok(Response::new(reply))
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let addr = "localhost:20007".parse()?;
+    let server = Heart7D::default();
+
+    Server::builder()
+        .add_service(Heart7Server::new(server))
+        .serve(addr)
+        .await?;
+
+    Ok(())
 }
