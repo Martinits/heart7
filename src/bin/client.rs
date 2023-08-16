c393dcbbb81f4c6ae038442a848ebc11b9ed90b3
Martinits <mamartinitramam@163.com>
2023-08-16 22:19:38 +0800

    add logging

--- src/bin/client.rs
+++ src/bin/client.rs
@@ -6,9 +6,24 @@ use std::io;
 use ratatui::backend::CrosstermBackend;
 use ratatui::Terminal;
 use tokio_util::sync::CancellationToken;
+use log::LevelFilter;
+use log::{info, debug, warn, error};
+use log4rs::append::file::FileAppender;
+use log4rs::config::{Appender, Config, Root};
 
 #[tokio::main]
 async fn main() -> AppResult<()> {
+
+    let logfile = FileAppender::builder().build("heart7.log")?;
+    let config = Config::builder()
+        .appender(Appender::builder().build("logfile", Box::new(logfile)))
+        .build(Root::builder()
+                   .appender("logfile")
+                   .build(LevelFilter::Info))?;
+    log4rs::init_config(config)?;
+
+    info!("Heart7 Client Starts!");
+
     let cancel = CancellationToken::new();
 
     let backend = CrosstermBackend::new(io::stdout());
