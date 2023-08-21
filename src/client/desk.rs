3b68d153857ee4eb0e7592862304dc2316a7b3a5
Martinits <mamartinitramam@163.com>
2023-08-21 15:40:44 +0800

    impl desk update algo

--- src/client/desk.rs
+++ src/client/desk.rs
@@ -5,81 +5,14 @@ use crate::*;
 pub struct Desk {
     // (cards<=7, cards>7), index == 0: min or max cardnum
     // bool means whether it is in this round
-    spade:   (Vec<(Card, bool)>, Vec<(Card, bool)>),
-    heart:   (Vec<(Card, bool)>, Vec<(Card, bool)>),
-    club:    (Vec<(Card, bool)>, Vec<(Card, bool)>),
-    diamond: (Vec<(Card, bool)>, Vec<(Card, bool)>),
-    // bool: false => 0, true => 1 in tuple
-    // usize: idx of Vec
-    round:   Vec<(CardSuit, bool, usize)>
+    pub spade:   (Vec<(Card, bool)>, Vec<(Card, bool)>),
+    pub heart:   (Vec<(Card, bool)>, Vec<(Card, bool)>),
+    pub club:    (Vec<(Card, bool)>, Vec<(Card, bool)>),
+    pub diamond: (Vec<(Card, bool)>, Vec<(Card, bool)>),
 }
 
 impl Desk {
-    pub fn update(&mut self, c: Card, new_round: bool) {
-        if new_round {
-            for each in self.round.iter() {
-                match each {
-                    (CardSuit::Spade, false, idx) => {
-                        if *idx == 0 {
-                            self.spade.0[0].1 = false;
-                        } else {
-                            self.spade.0.remove(*idx);
-                        }
-                    }
-                    (CardSuit::Spade,true, idx) => {
-                        if *idx == 0 {
-                            self.spade.1[0].1 = false;
-                        } else {
-                            self.spade.1.remove(*idx);
-                        }
-                    }
-                    (CardSuit::Heart, false, idx) => {
-                        if *idx == 0 {
-                            self.heart.0[0].1 = false;
-                        } else {
-                            self.heart.0.remove(*idx);
-                        }
-                    }
-                    (CardSuit::Heart, true, idx) => {
-                        if *idx == 0 {
-                            self.heart.1[0].1 = false;
-                        } else {
-                            self.heart.1.remove(*idx);
-                        }
-                    }
-                    (CardSuit::Club, false, idx) => {
-                        if *idx == 0 {
-                            self.club.0[0].1 = false;
-                        } else {
-                            self.club.0.remove(*idx);
-                        }
-                    }
-                    (CardSuit::Club, true, idx) => {
-                        if *idx == 0 {
-                            self.club.1[0].1 = false;
-                        } else {
-                            self.club.1.remove(*idx);
-                        }
-                    }
-                    (CardSuit::Diamond, false, idx) => {
-                        if *idx == 0 {
-                            self.diamond.0[0].1 = false;
-                        } else {
-                            self.diamond.0.remove(*idx);
-                        }
-                    }
-                    (CardSuit::Diamond, true, idx) => {
-                        if *idx == 0 {
-                            self.diamond.1[0].1 = false;
-                        } else {
-                            self.diamond.1.remove(*idx);
-                        }
-                    }
-                }
-            }
-            self.round.clear();
-        }
-
+    pub fn add(&mut self, c: Card) {
         let target: &mut Vec<(Card, bool)> = match (c.suit, c.num <= 7) {
             (CardSuit::Spade,   true) => self.spade.0.as_mut(),
             (CardSuit::Spade,   false) => self.spade.1.as_mut(),
@@ -91,10 +24,27 @@ impl Desk {
             (CardSuit::Diamond, false) => self.diamond.1.as_mut(),
         };
 
-        target.insert(0, (c, true));
-        if target.len() > 1 && !target[1].1 {
-            assert!(target.len() == 2);
-            target.pop();
+        assert!(target.len() == 0 || (target[0].0.num.abs_diff(7) < c.num.abs_diff(7)));
+
+        if target.len() > 0 && !target[0].1 {
+            assert!(target.len() == 1);
+            target[0] = (c, true);
+        } else {
+            target.insert(0, (c, true));
+        }
+    }
+
+    pub fn new_round(&mut self) {
+        for each in [
+            &mut self.spade.0, &mut self.spade.1,
+            &mut self.heart.0, &mut self.heart.1,
+            &mut self.club.0, &mut self.club.1,
+            &mut self.diamond.0, &mut self.diamond.1,
+        ] {
+            if each.len() > 0 {
+                each.iter_mut().for_each( |(_, r)| *r = false );
+                each.truncate(1);
+            }
         }
     }
 }
