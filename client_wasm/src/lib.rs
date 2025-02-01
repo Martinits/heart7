97df271dc087e6f5324ee426a84c9f3079cd2a4b
Martinits <mamartinitramam@163.com>
2025-02-01 19:13:52 +0800

    client_wasm: main structures

--- client_wasm/src/lib.rs
+++ client_wasm/src/lib.rs
@@ -1,16 +1,168 @@
-mod game;
-mod rpc;
-pub use game::*;
-pub use rpc::*;
+mod event;
+mod ui;
+pub use event::*;
+pub use ui::*;
 
 use wasm_bindgen::prelude::*;
 use wasm_bindgen_futures::spawn_local;
+pub use tonic::{Code, Request, Status};
+use tonic_web_wasm_client::Client;
+pub use ::heart7_client::*;
+use std::net::Ipv4Addr;
+use async_channel::{bounded, Sender, Receiver};
 
 pub(crate) type JsResult<T> = Result<T, JsValue>;
 
+pub const DEFAULT_IP: &str = "127.0.0.1";
+pub const DEFAULT_PORT: u16 = 20007;
+
+pub const DEFAULT_CHANNEL_SIZE: usize = 64;
+
+fn build_client(addr: String) -> Result<RpcClient, String> {
+    let (ip, port): (String, String) = match addr.find(':') {
+        Some(i) => (addr[0..i].into(), addr[i+1..].into()),
+        None => ("".into(), "".into())
+    };
+
+    if ip.len() == 0 || port.len() == 0 {
+        Err("Invalid ip or port!".into())
+    } else if !ip.parse::<Ipv4Addr>().is_ok() {
+        Err("Invalid ip address!".into())
+    } else {
+        let url = format!("http://{}", &addr);
+        let web_client = Client::new(url);
+        Ok(RpcClient::new(Heart7Client::new(web_client), addr))
+    }
+}
+
+fn spawn_event_handler(tx: Sender<ClientEvent>) {
+    info!("Starting event handler...");
+    unimplemented!();
+}
+
+struct StreamCancelToken;
+
+struct ClientWasm {
+    c: ClientStateManager,
+    tx: Sender<ClientEvent>,
+    rx: Receiver<ClientEvent>,
+    stream_tx: Sender<StreamCancelToken>,
+    stream_rx: Receiver<StreamCancelToken>,
+}
+
+impl ClientWasm {
+    pub fn new(default_addr: String) -> Self {
+        let (tx, rx) = bounded(DEFAULT_CHANNEL_SIZE);
+        let (stream_tx, stream_rx) = bounded(2);
+
+        Self {
+            c: ClientStateManager::new(default_addr),
+            tx,
+            rx,
+            stream_tx,
+            stream_rx,
+        }
+    }
+
+    fn spawn_stream_listener(&mut self, mut gs: GameStream) {
+        let txc = self.tx.clone();
+        let cancel_rx = self.stream_rx.clone();
+        info!("Spawning GameStream listener...");
+        spawn_local(async move {
+            txc.send(ClientEvent::StreamListenerSpawned).await
+                .expect("Send Action::StreamListenerSpawned to client");
+            loop {
+                if let Ok(_) = cancel_rx.try_recv() {
+                    break;
+                }
+                match gs.message().await {
+                    Err(s) => panic!("GameStream error: {}", s),
+                    Ok(None) => {
+                        info!("GameStream closed! Stream listener exits!");
+                        break;
+                    }
+                    Ok(Some(msg)) => txc.send(ClientEvent::StreamMsg(msg)).await
+                        .expect("Send Action::StreamMsg to client"),
+                }
+            }
+        });
+    }
+
+    fn spawn_rpc_client(&mut self, addr: String) {
+        let txc = self.tx.clone();
+        spawn_local(async move {
+            let c = build_client(addr);
+            txc.send(ClientEvent::ServerConnectResult(c))
+                .await.expect("Send Action::ServerConnectResult to client");
+        });
+    }
+
+    pub async fn run(&mut self) -> JsResult<()> {
+        spawn_event_handler(self.tx.clone());
+
+        // draw first anyway
+        self.draw()?;
+        loop {
+            match self.rx.recv().await {
+                Err(e) => {
+                    warn!("channel to client closed: {}", e);
+                    break;
+                }
+                Ok(e) => {
+                    let reply = self.c.advance(e, should_block()).await;
+                    if reply.full_exit {
+                        // exit the "run" function only,
+                        // the "exit" function will do the cancelling later.
+                        break;
+                    }
+                    if reply.cancel_stream_listener {
+                        self.cancel_stream_listener();
+                    }
+                    if let Some(addr) = reply.spawn_rpc_client {
+                        self.spawn_rpc_client(addr);
+                    }
+                    if let Some(gs) = reply.spawn_stream_listener {
+                        self.spawn_stream_listener(gs);
+                    }
+                    if reply.need_redraw {
+                        self.draw()?;
+                    }
+                }
+            }
+        }
+
+        Ok(())
+    }
+
+    fn draw(&mut self) -> JsResult<()> {
+        if should_block() {
+            draw_blocked()?;
+        } else {
+            draw()?;
+        }
+        Ok(())
+    }
+
+    fn cancel_stream_listener(&self) {
+        let _ = self.stream_tx.send(StreamCancelToken{});
+    }
+
+    pub fn exit(self) -> JsResult<()> {
+        self.cancel_stream_listener();
+        Ok(())
+    }
+}
+
+
 #[wasm_bindgen(start)]
 fn main() {
-    spawn_local(test_rpc());
+    spawn_local(async move {
+        let mut client = ClientWasm::new(format!("{}:{}", DEFAULT_IP, DEFAULT_PORT));
 
-    start_game().unwrap_throw();
+        info!("Heart7 Client Starts!");
+        client.run().await.expect("Running ClientWasm");
+
+        info!("Exiting...");
+        client.exit().expect("Exiting ClientWasm");
+    });
 }
---
+++ client_wasm/src/ui/color.rs
@@ -0,0 +1,53 @@
+pub const BORDER_DARK:           &str = "#884A39";
+pub const BORDER_LIGHT:          &str = "#C38154";
+pub const BORDER_NORMAL:         &str = "#A47E3B";
+
+pub const TEXT_NORMAL:           &str = "#F49D1A";
+pub const INPUT_BORDER:          &str = "#EFB036";
+pub const INPUT_BORDER_BLOCK:    &str = "#F2EFE7";
+
+pub const BUTTON:                &str = "#E6B325";
+pub const BUTTON_DIM:            &str = "#C8C2BC";
+
+pub const CENTER_MSG:            &str = "#FF5B00";
+
+pub const CARD_SIGN:             &str = "#73777B";
+
+pub const NAME:                  &str = "#FAC213";
+pub const NAME_DIM:              &str = "#0xE8B0";
+
+pub const NORMAL_DIM:            &str = "#B7CADB";
+
+pub const READY:                 &str = "#95CD41";
+
+pub const HOLD_NUM:              &str = "#BB371A";
+
+pub const CARD_BORDER:           &str = "#AAAAAA";
+pub const CARD_HIGHLIGHT:        &str = "#FFC26F";
+pub const CARD_HIGHLIGHT_MY:     &str = "#95CD41";
+pub const CARD_CLEAR_BOREDER:    &str = "#2CD3E1";
+pub const MYCARD_BORDER:         &str = "#D5D3B3";
+pub const MYCARD_BORDER_DIM:     &str = "#7E8A97";
+#[allow(unused)]
+pub const CARD_HOLD:             &str = "#F6FA70";
+pub const CARD_CLEAR:            &str = "#B3FFAE";
+
+pub const SPADE:                 &str = "#0x6C5D";
+pub const HEART:                 &str = "#FA1E0E";
+pub const CLUB:                  &str = "#999999";
+pub const DIAMOND:               &str = "#FF7B54";
+
+pub const HOLD_BORDER:           &str = "#FF7777";
+
+pub const NEXT_TURN:             &str = "#FF6701";
+
+pub const GAME_MSG:              &str = "#F90716";
+
+pub const DESK_RESULT_0:         &str = "#95CD41";
+pub const DESK_RESULT_1:         &str = "#F90716";
+pub const DESK_RESULT_2:         &str = "#FFCE45";
+pub const DESK_RESULT_3:         &str = "#35D0BA";
+
+pub const RESULT_MSG_LOSE:       &str = "#0x0FA2";
+pub const RESULT_MSG_WIN:        &str = "#FF0060";
+pub const RESULT_MSG_GREY:       &str = "#C1C1C1";
