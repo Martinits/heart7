dbe274de133d4fdd405af2182aadd5c1c0e5ff1a
Martinits <mamartinitramam@163.com>
2025-02-01 16:25:21 +0800

    client_wasm: main structures

--- client_wasm/src/ui/mod.rs
+++ client_wasm/src/ui/mod.rs
@@ -1,18 +1,16 @@
+mod color;
+
 use wasm_bindgen::prelude::*;
 use wasm_bindgen::JsCast;
 use web_sys::{CanvasRenderingContext2d, HtmlCanvasElement};
 use crate::*;
-
-const RED: &str = "#AA0000";
-
-#[global_allocator]
-static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+use color::*;
 
 fn draw_rounded_rect(
     ctx: &CanvasRenderingContext2d, x: f64, y: f64, width: f64, height: f64, radius: f64
 ) -> JsResult<()> {
     ctx.begin_path();
-    ctx.set_stroke_style_str(RED);
+    ctx.set_stroke_style_str(CARD_BORDER);
     ctx.move_to(x + radius, y);
     ctx.arc_to(x + width, y, x + width, y + radius, radius)?;
     ctx.arc_to(x + width, y + height, x + width - radius, y + height, radius)?;
@@ -23,7 +21,7 @@ fn draw_rounded_rect(
     Ok(())
 }
 
-pub fn start_game() -> Result<(), JsValue> {
+pub fn draw() -> JsResult<()> {
     let window = web_sys::window().unwrap();
     let canvas: HtmlCanvasElement = window
         .document()
@@ -40,4 +38,13 @@ pub fn start_game() -> Result<(), JsValue> {
     draw_rounded_rect(&context, 400f64, 300f64, 80f64, 60f64, 5f64)?;
 
     Ok(())
+}
+
+pub fn draw_blocked() -> JsResult<()> {
+    Ok(())
+}
+
+pub fn should_block() -> bool {
+    // TODO: should_block
+    false
 }
